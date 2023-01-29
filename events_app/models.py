ebd27777aa6cec7f7d6ed760afafe4fb345833e4
scottzyang <scottzyang@gmail.com>
2023-01-29 13:31:44 -0600

    Create Event and Guest models

--- events_app/models.py
+++ events_app/models.py
@@ -11,8 +11,16 @@ from sqlalchemy.orm import backref
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(80), nullable=False)
+    events_attending = db.relationship('Event', secondary='guest_event', back_populates='guests')
 
+    def __str__(self):
+        return f'<Guests: {self.name}>\n<Name: {self.email}>\n<Phone: {self.phone}>\n'
+
+    def __repr__(self):
+        return f'<Guests: {self.name}>'
 # TODO: Create a model called `Event` with the following fields:
 # - id: primary key
 # - title: String column
@@ -25,9 +33,21 @@ class Guest(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
+    date_and_time = db.Column(db.DateTime, nullable=False)
+    guests = db.relationship('Guest', secondary='guest_event', back_populates='events_attending')
+
+    def __str__(self):
+        return f'<Event: {self.title}>'
+
+    def __repr__(self):
+        return f'<Event: {self.title}>'
 
 # TODO: Create a table `guest_event_table` with the following columns:
 # - event_id: Integer column (foreign key)
 # - guest_id: Integer column (foreign key)
-
-guest_event_table = None
+guest_event_table = db.Table('guest_event',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'))
+)
